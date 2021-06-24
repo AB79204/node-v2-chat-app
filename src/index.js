2e1c8e77291bafd6beed21a605abedd8a1400729
Hanamantha Bhimappa <Hanamantha.Bhimappa@CenturyLink.com>
2021-06-24 16:37:27 +0530

    updated Admin

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ io.on('connection', (socket) => {
         }
         socket.join(user.room)
 
-        socket.emit('message', generateMessage(user.username, 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(user.username, `${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -54,7 +54,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if(user){
-            io.to(user.room).emit('message', generateMessage(user.username, `${user.username} has left`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left`))
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
